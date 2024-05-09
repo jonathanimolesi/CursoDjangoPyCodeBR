2bdee8751a1f2d4bfa9ce28877dfcc7a56569373
jonathanimolesi <jonjonimolesi@gmail.com>
2024-05-09 16:39:52 -0300

    aula 48

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,10 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.all()
+    #cars = Car.objects.all() #mostra todos
+    #cars = Car.objects.filter(brand__name= 'FIAT') #filtrando brand com nome FIAT case sensitive
+    #cars = Car.objects.filter(brand=1) #filtrando pelo id da tabela
+    cars = Car.objects.filter(model__contains='x') #filtrando por x que contenha no nome, não é case sensitive
 
     return render(request, 'cars.html', {'cars': cars})
 
